3ea09560f46d4e634e36e21a92c4d22889fabd98
cosimo-schiavoni <82418602+cosimo-schiavoni@users.noreply.github.com>
2021-04-13 01:09:46 +0200

    Add files via upload

--- EKC_CURVE_CHINA.R
+++ EKC_CURVE_CHINA.R
@@ -1,1 +1,21 @@
-print("HEllo")
+library(shiny)
+
+# Define UI for miles per gallon app ----
+ui <- pageWithSidebar(
+  
+  # App title ----
+  headerPanel("EKC CURVE CHINA"),
+  
+  # Sidebar panel for inputs ----
+  sidebarPanel(),
+  
+  # Main panel for displaying outputs ----
+  mainPanel()
+)
+
+# Define server logic to plot various variables against mpg ----
+server <- function(input, output) {
+  
+}
+
+shinyApp(ui, server)
